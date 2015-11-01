dbe24463cd53314b46c78d56eb17b29591b828be
Ralph Pina <ralph.pina@gmail.com>
2015-11-01 08:44:34 -0800

    Added Parse logout

--- mobile/src/main/java/net/ralphpina/activitymapper/MainActivity.java
+++ mobile/src/main/java/net/ralphpina/activitymapper/MainActivity.java
@@ -1,7 +1,9 @@
 package net.ralphpina.activitymapper;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.annotation.IntDef;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +11,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.parse.LogOutCallback;
+import com.parse.ParseException;
 import com.parse.ParseUser;
 
 import net.ralphpina.activitymapper.events.EventBusMethod;
@@ -84,9 +88,13 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        if (_uiState == MAP) {
+            // Inflate the menu; this adds items to the action bar if it is present.
+            getMenuInflater().inflate(R.menu.menu_main, menu);
+            return true;
+        } else {
+            return false;
+        }
     }
 
     @Override
@@ -96,8 +104,24 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_logout) {
+            final ProgressDialog dialog = new ProgressDialog(this);
+            dialog.setMessage(getString(R.string.progress_logout));
+            dialog.show();
+            ParseUser.logOutInBackground(new LogOutCallback() {
+                @Override
+                public void done(ParseException e) {
+                    if (e != null) {
+                        Snackbar.make(_toolbar,
+                                      e.getMessage(),
+                                      Snackbar.LENGTH_LONG)
+                                .show();
+                    } else {
+                        _uiState = LOGIN;
+                        navigateToFragment(BACKWARDS);
+                    }
+                }
+            });
             return true;
         }
 
@@ -117,6 +141,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void navigateToFragment(@TransitionDirection int direction) {
+        invalidateOptionsMenu();
         Fragment fragment = null;
         String tag = null;
         if (_uiState == MAP) {
